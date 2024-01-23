68158b6c73697087f39255a3a657863fe83412b3
KBeKind <79383539+KBeKind@users.noreply.github.com>
2024-01-23 09:22:40 -0600

    commit

--- src/app/components/employee/CreateEmployee.jsx
+++ src/app/components/employee/CreateEmployee.jsx
@@ -3,10 +3,11 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-const CreateProduct = () => {
+const CreateEmployee = () => {
   const [formData, setFormData] = useState({
     name: "",
-    price: "",
+    department: "",
+    city: "",
   });
 
   const handleChange = (e) => {
@@ -22,12 +23,13 @@ const CreateProduct = () => {
 
     const payload = {
       name: formData.name,
-      price: formData.price,
+      department: formData.department,
+      city: formData.city,
     };
 
     try {
       const response = await axios.post(
-        `https://localhost:5001/api/Product`,
+        `https://localhost:5001/api/Employee`,
         payload,
         {
           headers: {
@@ -39,19 +41,19 @@ const CreateProduct = () => {
       );
 
       if (response.status === 201) {
-        console.log("Product created successfully");
-        window.location.href = "/products";
+        console.log("Employee created successfully");
+        window.location.href = "/employees";
       } else {
-        console.error("Failed to create product");
+        console.error("Failed to create employee");
       }
     } catch (error) {
-      console.error("Error creating product:", error);
+      console.error("Error creating employee:", error);
     }
   };
 
   return (
     <div className="p-4 border-solid border-2 border-slate-600 rounded-md bg-slate-800 w-full flex flex-col items-center">
-      <p className="text-center text-lg mb-2">Create Product</p>
+      <p className="text-center text-lg mb-2">Create Employee</p>
       <form onSubmit={handleSubmit} className=" w-full max-w-md items-center">
         <div className="mb-4">
           <label htmlFor="name">Name:</label>
@@ -66,12 +68,24 @@ const CreateProduct = () => {
           />
         </div>
         <div className="mb-4">
-          <label htmlFor="price">Price:</label>
+          <label htmlFor="department">Department:</label>
           <input
-            type="number"
-            id="price"
-            name="price"
-            placeholder="price"
+            type="text"
+            id="department"
+            name="department"
+            placeholder="department"
+            className="w-full p-1 border rounded text-slate-500"
+            value={formData.price}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="city">City:</label>
+          <input
+            type="text"
+            id="city"
+            name="city"
+            placeholder="city"
             className="w-full p-1 border rounded text-slate-500"
             value={formData.price}
             onChange={handleChange}
@@ -82,7 +96,7 @@ const CreateProduct = () => {
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-3/4"
             type="submit"
           >
-            Save Product
+            Save Employee
           </button>
         </div>
       </form>
@@ -90,4 +104,4 @@ const CreateProduct = () => {
   );
 };
 
-export default CreateProduct;
+export default CreateEmployee;
